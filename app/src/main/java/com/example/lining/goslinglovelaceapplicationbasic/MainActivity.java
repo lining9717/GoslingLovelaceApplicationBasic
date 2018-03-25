e57c1a0afd2766171e24c2dadd885859d94028fc
lining <980994630@qq.com>
2018-03-25 15:46:36 +0800

    commit Part 1

---
+++ app/src/main/java/com/example/lining/goslinglovelaceapplicationbasic/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lining.goslinglovelaceapplicationbasic;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        if(isScrennLand()){
+            setContentView(R.layout.activity_main);
+        }else {
+            setContentView(R.layout.activity_main_land);
+        }
+    }
+
+
+    private boolean isScrennLand() {
+        // 横屏
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            return true;
+        }
+        return false;
+    }
+}
